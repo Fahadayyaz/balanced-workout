a582ec26a10ba440840882bd0cfadba4df3c30c4
MehfoozurRehman <mehfoozijaz786@gmail.com>
2024-05-06 12:05:23 +0500

    Update SwipeButton.js

--- components/SwipeButton.js
+++ components/SwipeButton.js
@@ -1,38 +1,30 @@
-import {
-  Animated,
-  Dimensions,
-  PanResponder,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { Animated, Dimensions, PanResponder, Text } from "react-native";
+import { memo, useRef } from "react";
 
-import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
-const { width } = Dimensions.get("window");
+const { width: windowWidth } = Dimensions.get("window");
+
 const SWIPE_THRESHOLD = 100;
 
-const SwipeButton = () => {
-  const pan = React.useRef(new Animated.ValueXY()).current;
+const SwipeButton = ({ callback }) => {
+  const pan = useRef(new Animated.ValueXY()).current;
 
-  const panResponder = React.useRef(
+  const panResponder = useRef(
     PanResponder.create({
-      onStartShouldSetPanResponder: () => true,
-      onPanResponderMove: (event, gestureState) => {
-        // Restricting movement to horizontal direction only
-        Animated.event([null, { dx: pan.x }])(event, gestureState);
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: (_, gestureState) => {
+        pan.setValue({ x: gestureState.dx, y: 0 });
       },
-      onPanResponderRelease: (event, gestureState) => {
+      onPanResponderRelease: (_, gestureState) => {
         if (gestureState.dx > SWIPE_THRESHOLD) {
-          console.log("Swipe gesture detected! Powering off...");
-          // Implement logic for powering off here
-          // For now, let's just reset the position of the swipe button
+          callback();
+
           Animated.spring(pan, {
-            toValue: { x: 0, y: 0 },
+            toValue: { x: windowWidth * 0.9 - 60, y: 0 },
             useNativeDriver: false,
           }).start();
         } else {
-          // Snap back to the original position if not swiped enough
           Animated.spring(pan, {
             toValue: { x: 0, y: 0 },
             useNativeDriver: false,
@@ -43,42 +35,35 @@ const SwipeButton = () => {
   ).current;
 
   return (
-    <View style={styles.container}>
+    <LinearGradient
+      colors={["transparent", "#ffffffa0"]}
+      start={{ x: 0.9, y: 0 }}
+      end={{ x: 0.9, y: 0 }}
+      style={{
+        marginTop: 40,
+        marginLeft: 20,
+        width: windowWidth * 0.9,
+        height: 60,
+        borderRadius: 50,
+        borderWidth: 1,
+        padding: 5,
+      }}
+    >
       <Animated.View
-        style={[
-          styles.button,
-          {
-            transform: [{ translateX: pan.x }],
-          },
-        ]}
+        style={[{ transform: [{ translateX: pan.x }] }]}
         {...panResponder.panHandlers}
       >
-        <Text style={styles.text}>Swipe this</Text>
+        <LinearGradient
+          colors={["#ffffff", "transparent"]}
+          start={{ x: 0.5, y: 0.5 }}
+          end={{ x: 1, y: 1 }}
+          style={{ width: 50, height: 50, borderRadius: 50 }}
+        >
+          <Text style={{ color: "#ffffff" }}>{">"}</Text>
+        </LinearGradient>
       </Animated.View>
-    </View>
+    </LinearGradient>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  button: {
-    backgroundColor: "#FFFFFF",
-    width: width - 20,
-    height: 60,
-    borderRadius: 30,
-    justifyContent: "center",
-    alignItems: "center",
-    elevation: 3,
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "#333",
-  },
-});
-
-export default SwipeButton;
+export default memo(SwipeButton);
