18813c4ee2e043290c18ba27cb0d41823519bc74
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-05-29 18:43:38 +0500

    home updated

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import Age from "./screens/Age";
 import Weight from "./screens/Weight";
 import Height from "./screens/Height";
 import Goal from "./screens/Goal";
+import TabNav from "./components/TabNav";
 import PhysicalActivity from "./screens/PhysicalActivity";
 import ProfilePicture from "./screens/ProfilePicture";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -41,7 +42,7 @@ const App = () => {
         <Stack.Screen name="Weight" component={Weight} />
         <Stack.Screen name="Height" component={Height} />
         <Stack.Screen name="Goal" component={Goal} />
-
+        <Stack.Screen name="TabNav" component={TabNav} />
         <Stack.Screen name="PhysicalActivity" component={PhysicalActivity} />
         <Stack.Screen name="ProfilePicture" component={ProfilePicture} />
       </Stack.Navigator>
--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Image, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Pressable,
+  ImageBackground,
+  ScrollView,
+} from "react-native";
 import CircularProgress from "react-native-circular-progress-indicator";
 
 const Home = () => {
@@ -34,81 +42,154 @@ const Home = () => {
           />
         </View>
       </View>
-      <View style={styles.activityWrap}>
-        <View style={styles.activityText}>
-          <Text style={{ color: "#BDFE30", fontWeight: "bold", fontSize: 22 }}>
-            Today's Activity
+      <ScrollView style={styles.scrollViewContent}>
+        <View style={styles.content}>
+          <View style={styles.activityWrap}>
+            <View style={styles.activityText}>
+              <Text
+                style={{ color: "#BDFE30", fontWeight: "bold", fontSize: 22 }}
+              >
+                Today's Activity
+              </Text>
+              <Text style={{ color: "lightgrey" }}>2024 October 25</Text>
+            </View>
+            <View style={styles.progressCircle}>
+              <CircularProgress
+                radius={50}
+                value={value}
+                textColor="#222"
+                fontSize={16}
+                valueSuffix={"%"}
+                inActiveStrokeColor={"#E2FFF5"}
+                inActiveStrokeOpacity={0.5}
+                inActiveStrokeWidth={30}
+                activeStrokeColor={"#BDFE30"}
+                activeStrokeWidth={30}
+                duration={2000}
+                showProgressValue={false}
+                onAnimationComplete={() => setValue(50)}
+              />
+            </View>
+          </View>
+          <View style={styles.activeWorkoutAndChallengesWrap}>
+            {/* Active Workout card starts here */}
+            <View style={styles.activeWorkout}>
+              <Image
+                source={require("../assets/homeAssets/navArrow.png")}
+                style={styles.navArrow}
+              />
+              <Image
+                source={require("../assets/homeAssets/activeWorkout.png")}
+                style={{
+                  width: 82,
+                  height: 63,
+                  position: "absolute",
+                  bottom: 0,
+                  margin: 50,
+                }}
+              />
+              <Text style={styles.cardsText}>Active Workout</Text>
+            </View>
+            {/* challenges card starts here */}
+            <View style={styles.challenges}>
+              <Image
+                source={require("../assets/homeAssets/navArrow.png")}
+                style={styles.navArrow}
+              />
+              <Image
+                source={require("../assets/homeAssets/challenges.png")}
+                style={{
+                  width: 82,
+                  height: 63,
+                  position: "absolute",
+                  bottom: 0,
+                  margin: 50,
+                }}
+              />
+              <Text style={styles.cardsText}>Challenges</Text>
+            </View>
+          </View>
+          <Pressable
+            style={{
+              width: "100%",
+              height: 50,
+              backgroundColor: "#BDFE30",
+              borderRadius: 74,
+              marginTop: "5%",
+              justifyContent: "center",
+              alignSelf: "center",
+            }}
+          >
+            <Text
+              style={{
+                color: "black",
+                alignSelf: "center",
+                fontWeight: "bold",
+                fontSize: 16,
+              }}
+            >
+              Create Feed
+            </Text>
+          </Pressable>
+          <Text style={{ color: "#D4D4D4", fontSize: 22, marginTop: 10 }}>
+            Recent Challenges
           </Text>
-          <Text style={{ color: "lightgrey" }}>2024 October 25</Text>
+          <ImageBackground
+            source={require("../assets/homeAssets/weightLifting.png")}
+            style={{ marginTop: 10, height: 195 }}
+          >
+            <View style={{ marginLeft: 23, marginTop: 63 }}>
+              <Text style={{ color: "#BDFE30", fontSize: 12 }}>
+                7x4 Challenge
+              </Text>
+              <Text style={{ color: "#fff", fontSize: 18, marginTop: 10 }}>
+                Simply Chest Workout
+              </Text>
+              <Pressable
+                style={{
+                  backgroundColor: "#BDFE30",
+                  width: 104,
+                  height: 35,
+                  justifyContent: "center",
+                  borderRadius: 40,
+                  marginTop: 15,
+                }}
+              >
+                <Text style={{ fontSize: 14, alignSelf: "center" }}>
+                  Start Now
+                </Text>
+              </Pressable>
+            </View>
+          </ImageBackground>
+          <ImageBackground
+            source={require("../assets/homeAssets/weightLifting.png")}
+            style={{ marginTop: 10, height: 195 }}
+          >
+            <View style={{ marginLeft: 23, marginTop: 63 }}>
+              <Text style={{ color: "#BDFE30", fontSize: 12 }}>
+                7x4 Challenge
+              </Text>
+              <Text style={{ color: "#fff", fontSize: 18, marginTop: 10 }}>
+                Simply Chest Workout
+              </Text>
+              <Pressable
+                style={{
+                  backgroundColor: "#BDFE30",
+                  width: 104,
+                  height: 35,
+                  justifyContent: "center",
+                  borderRadius: 40,
+                  marginTop: 15,
+                }}
+              >
+                <Text style={{ fontSize: 14, alignSelf: "center" }}>
+                  Start Now
+                </Text>
+              </Pressable>
+            </View>
+          </ImageBackground>
         </View>
-        <View style={styles.progressCircle}>
-          <CircularProgress
-            radius={50}
-            value={value}
-            textColor="#222"
-            fontSize={16}
-            valueSuffix={"%"}
-            inActiveStrokeColor={"#E2FFF5"}
-            inActiveStrokeOpacity={0.5}
-            inActiveStrokeWidth={30}
-            activeStrokeColor={"#BDFE30"}
-            activeStrokeWidth={30}
-            duration={2000}
-            showProgressValue={false}
-            onAnimationComplete={() => setValue(50)}
-          />
-        </View>
-      </View>
-      <View style={styles.activeWorkoutAndChallengesWrap}>
-        {/* Active Workout card starts here */}
-        <View style={styles.activeWorkout}>
-          <Image
-            source={require("../assets/homeAssets/navArrow.png")}
-            style={styles.navArrow}
-          />
-          <Image
-            source={require("../assets/homeAssets/activeWorkout.png")}
-            style={{
-              width: 82,
-              height: 63,
-              position: "absolute",
-              bottom: 0,
-              margin: 50,
-            }}
-          />
-          <Text style={styles.cardsText}>Active Workout</Text>
-        </View>
-        {/* challenges card starts here */}
-        <View style={styles.challenges}>
-          <Image
-            source={require("../assets/homeAssets/navArrow.png")}
-            style={styles.navArrow}
-          />
-          <Image
-            source={require("../assets/homeAssets/challenges.png")}
-            style={{
-              width: 82,
-              height: 63,
-              position: "absolute",
-              bottom: 0,
-              margin: 50,
-            }}
-          />
-          <Text style={styles.cardsText}>Challenges</Text>
-        </View>
-      </View>
-      <Pressable
-        style={{
-          width: "90%",
-          height: 50,
-          backgroundColor: "#BDFE30",
-          borderRadius: 74,
-          marginTop: "5%",
-          justifyContent: "center",
-        }}
-      >
-        <Text style={{ color: "black", alignSelf: "center" }}>Create Feed</Text>
-      </Pressable>
+      </ScrollView>
     </View>
   );
 };
@@ -117,13 +198,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#161616",
-    alignItems: "center",
   },
   profileWrap: {
     flexDirection: "row",
-    marginTop: "15%",
+    marginTop: "11%",
     height: 60,
     alignItems: "center",
+    alignSelf: "center",
   },
   profilePicBox: {
     width: "25%",
@@ -158,6 +239,14 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 24,
   },
+  scrollViewContent: {
+    height: "100%",
+  },
+  content: {
+    width: 373,
+    alignSelf: "center",
+    // backgroundColor: "brown",
+  },
   activityWrap: {
     marginTop: "5%",
     width: 373,
@@ -165,6 +254,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     backgroundColor: "#1E1E1E",
     borderRadius: 24,
+    alignSelf: "center",
   },
   activityText: {
     paddingTop: "10%",
@@ -183,6 +273,7 @@ const styles = StyleSheet.create({
     backgroundColor: "transparent",
     borderRadius: 24,
     justifyContent: "space-between",
+    alignSelf: "center",
   },
   activeWorkout: {
     backgroundColor: "#1E1E1E",
